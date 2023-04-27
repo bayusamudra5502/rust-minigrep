ba72a8e8e329b73c5bb8e2155e41246d7d0e54ab
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2023-04-27 22:07:27 +0700

    feat: add rust miniprogram

---
+++ src/config.rs
@@ -0,0 +1,46 @@
+use std::env;
+
+pub struct Config<'a> {
+  pub query: &'a str,
+  pub file_path: &'a str,
+  pub ignore_case: bool
+}
+
+impl<'a> Config<'a> {
+    pub fn new(query: &'a str, file_path: &'a str, ignore_case: bool) -> Config<'a> {
+      Config {
+        query,
+        file_path,
+        ignore_case
+      }
+    }
+
+    pub fn parse(args: &[String]) -> Result<Config, &str> {
+      if args.len() < 3 {
+        return Err("not enough arguments");
+      }
+    
+      let mut query: Result<&str, &str> = Err("query is empty");
+      let mut file_path: Result<&str, &str> = Err("file path is empty");
+      let mut ignore_case = env::var("IGNORE_CASE").is_ok();
+      let mut idx = 0;
+
+      for word in args {
+        if word == "--ignore-case" {
+          ignore_case = true;
+          continue;
+        } else if word == "--case-sensitive" {
+          ignore_case = false;
+          continue;
+        } else if idx == 1 {
+            query = Ok(&word);
+        } else if idx == 2 {
+            file_path = Ok(word);
+        }
+
+        idx += 1;
+      }
+    
+      Ok(Config::new(query?, file_path?, ignore_case))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::{env, process};
+
+use minigrep::config::Config;
+use minigrep::run::run;
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::parse(&args).unwrap_or_else(|err| {
+        eprintln!("Usage : minigrep <query> <filepath>");
+        eprintln!("{}", err);
+        process::exit(1);
+    });
+
+    if let Err(err) = run(config) {
+        eprintln!("Error : {}", err);
+        process::exit(-1);
+    }
 }
